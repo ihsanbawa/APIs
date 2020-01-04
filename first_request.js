92c23a87ea18b298cfe1475d9611e8bdcbd42a65
ihsanbawa <59374466+ihsanbawa@users.noreply.github.com>
2020-01-04 14:37:44 -0600

    initial import of API folder

---
+++ first_request.js
@@ -0,0 +1,16 @@
+const request = require('request');
+request('https://jsonplaceholder.typicode.com/users/1', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
+
+
+// var reqeust = require('request');
+// request('http://www.google.com',function(error,response, body){
+//     if(!error && response.statusCode == 200){
+//         // var parsedData = JSON.parse(body);
+//         // console.log(parsedData[name]);
+//         console.log(body);
+//     }
+// });
